6fd55feae62aa4dc85668730984dca6ce200c4ed
Jonathan Alexander <me@jmalexan.com>
2018-02-21 19:00:59 -0800

    Remove output list

--- src/app/term.service.ts
+++ src/app/term.service.ts
@@ -10,7 +10,7 @@ export class TermService {
 
   execute(cmd: string) {
     if (cmd.toUpperCase() == "HOME") {
-      this.displayText.push(`
+      this.displayText[0] = `
         # Welcome!
 
         This website is navigated by commands, type one in to get the info!
@@ -19,9 +19,9 @@ export class TermService {
 
         For info about my personal projects, run \`projects\`!
 
-        \\>jonathan_`);
+        \\>jonathan_`;
     } else if (cmd.toUpperCase() == "PROJECTS") {
-      this.displayText.push(`
+      this.displayText[0] = `
         # Personal Projects
 
         [Junction Manager](https://github.com/notdisliked/JunctionManager) is an application I created to solve a problem I had with managing what content was on what drive of my multiple drive PC. It lets you move folders back and forth between the drives with ease using Windows' Junctions, as well as helping you keep track and manage all the junctions created using the app.
@@ -30,9 +30,9 @@ export class TermService {
 
         [Contrast](https://tms.ogpc.info/Games/Details/72d4ac5f-d37b-4ac2-a11a-495f4da06174) is a game created during the 2015-2016 session of the Oregon Game Project Challenge. Made using Unity and C#, it revolves around creating portals which alter objects' collision box to avoid obstacles and solve puzzles.
 
-        [Vision](https://tms.ogpc.info/Games/Details/fb0afbd3-095f-410f-8be5-ce41c38cccf2) was created during the 2016-2017 session of the Oregon Game Project Challenge. It is a game developed specifically for the HTC Vive, and utilizes the Vive's headtracking to use what the player can or cannot see to solve puzzles`);
+        [Vision](https://tms.ogpc.info/Games/Details/fb0afbd3-095f-410f-8be5-ce41c38cccf2) was created during the 2016-2017 session of the Oregon Game Project Challenge. It is a game developed specifically for the HTC Vive, and utilizes the Vive's headtracking to use what the player can or cannot see to solve puzzles`;
     } else if (cmd.toUpperCase() == "ABOUT") {
-      this.displayText.push(`
+      this.displayText[0] = `
         # Hey there!
 
         My name's Jonathan, I'm currently attending Oregon State University for a degree in Computer Science. My passion is programming, it's what I want to go into and what I do as a hobby. My interests within programming pertain to interface design and front end programming, but I don't restrict myself to that, I explore as much as I can! I can't put my name on anything big yet, but I have my own personal or collaborative projects which you can see on my Projects page. I've had a few internships and jobs here or there, I'll list all those below. Thanks for reading!
@@ -51,7 +51,7 @@ export class TermService {
 
         2016: Saturday Academy - Summer IT Intern
 
-        2015: Enli - Summer Intern`);
+        2015: Enli - Summer Intern`;
     }
     window.scrollTo(0, document.body.scrollHeight);
   }
